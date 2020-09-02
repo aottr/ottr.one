fb6514e567925d8f85b848a32f02202865c3193c
Dustin Kröger <alex@Dustins-MacBook-Pro.local>
2020-09-02 23:38:43 +0200

    introduce dotenv and ejs

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+// init config
+dotenv.config();
+
+const port = process.env.SERVER_PORT;
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('OK');
-});
+// configure express for ejs
+app.set('views', path.join( __dirname, 'views' ));
+app.set('view engine', 'ejs');
 
-app.listen(5000, () => console.log('Server startet'));
+app.get('/', (req: Request, res: Response) => {
+    res.render('index');
+}); 
+
+// start express server
+app.listen(port, () => {
+    console.log(`server startet at http://localhost:${ port }`)
+});
